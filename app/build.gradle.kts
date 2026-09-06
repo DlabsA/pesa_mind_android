799e1a8190f8825cce50d33e50e68579383b209a
ConradKash <kakurucon1234@gmail.com>
2026-09-06 22:42:43 +0300

    Bump version code and name to 37 in build configuration
    Signed-off-by: ConradKash <kakurucon1234@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = "cc.dlabs.pesamind"
         minSdk = 26
         targetSdk = 36
-        versionCode = 36
-        versionName = "36"
+        versionCode = 37
+        versionName = "37"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
